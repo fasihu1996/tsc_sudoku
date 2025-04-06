5af4512006495682fdd83cc13a2d3a732a994713
fasihu1996 <fasih1996@gmail.com>
2025-04-06 23:10:31 +0200

    CellValue removed

--- src/types/sudoku_types.ts
+++ src/types/sudoku_types.ts
@@ -1,9 +1,7 @@
 export type Difficulty = "easy" | "medium" | "hard";
 
-export type CellValue = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-
 export interface Cell {
+    position: [number, number];
     row: number;
     column: number;
-    value?: CellValue;
 }
