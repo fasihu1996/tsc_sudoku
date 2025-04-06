4616cde942369a5d25bac1ba9317b8cbe49ce4ea
fasihu1996 <fasih1996@gmail.com>
2025-04-06 15:34:34 +0200

    missing return statement, headline, paragraph added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,38 @@
 export default function Home() {
-    <div>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p>
-            Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam
-            varius, turpis et commodo pharetra, est eros bibendum elit, nec
-            luctus magna felis sollicitudin mauris. Integer in mauris eu nibh
-            euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec
-            lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu
-            tempor congue, eros est euismod turpis, id tincidunt sapien risus a
-            quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque
-            malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget,
-            consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet,
-            felis nisl adipiscing sapien, sed malesuada diam lacus eget erat.
-            Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat.
-            Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi.
-            Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In
-            hac habitasse platea dictumst.
-        </p>
-        <p>
-            Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis
-            suscipit mauris, sed placerat ipsum urna sed risus. Class aptent
-            taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-            hymenaeos. Nulla facilisi. Integer lacinia sollicitudin massa. Cras
-            metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi.
-            Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit
-            amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean
-            laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies
-            eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet
-            eget, justo. Sed pretium blandit orci. Ut eu diam at pede suscipit
-            sodales.
-        </p>
-    </div>;
+    return (
+        <div>
+            <h1>Welcome to the prettiest sudoku you ever played.</h1>
+            <p>This is the first paragraph.</p>
+            <p>
+                Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.
+                Nullam varius, turpis et commodo pharetra, est eros bibendum
+                elit, nec luctus magna felis sollicitudin mauris. Integer in
+                mauris eu nibh euismod gravida. Duis ac tellus et risus
+                vulputate vehicula. Donec lobortis risus a elit. Etiam tempor.
+                Ut ullamcorper, ligula eu tempor congue, eros est euismod
+                turpis, id tincidunt sapien risus a quam. Maecenas fermentum
+                consequat mi. Donec fermentum. Pellentesque malesuada nulla a
+                mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis,
+                neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl
+                adipiscing sapien, sed malesuada diam lacus eget erat. Cras
+                mollis scelerisque nunc. Nullam arcu. Aliquam consequat.
+                Curabitur augue lorem, dapibus quis, laoreet et, pretium ac,
+                nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in,
+                orci. In hac habitasse platea dictumst.
+            </p>
+            <p>
+                Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis
+                suscipit mauris, sed placerat ipsum urna sed risus. Class aptent
+                taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos hymenaeos. Nulla facilisi. Integer lacinia sollicitudin
+                massa. Cras metus. Sed aliquet risus a tortor. Integer id quam.
+                Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in,
+                ultrices sit amet, augue. Proin sodales libero eget ante. Nulla
+                quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac,
+                facilisis ac, ultricies eu, pede. Ut orci risus, accumsan
+                porttitor, cursus quis, aliquet eget, justo. Sed pretium blandit
+                orci. Ut eu diam at pede suscipit sodales.
+            </p>
+        </div>
+    );
 }
